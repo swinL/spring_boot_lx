120717594845a95a30625a7fd419ff187b5611dc
swinL <fchy01@163.com>
2018-12-03 10:18:07 +0800

    测试公司电脑修改提交用户

--- readinglist/src/main/java/com/manning/readinglist/SecurityConfig.java
+++ readinglist/src/main/java/com/manning/readinglist/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.manning.readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 
 public class SecurityConfig {
 }
